af0b1d798d9cc7c5ccffbba38f81ae061d8b5272
ht555-EXE <ht555@exeter.ac.uk>
2026-01-31 16:10:07 +0000

    wrapper for basic function

---
+++ gptWrapper.py
@@ -0,0 +1,24 @@
+from google import genai
+import os
+
+class Wrapper:
+    def __init__(self, system_prompt="You are emotionally supportive"):
+        self.client = genai.Client()
+        self.chat = self.client.chats.create(
+            model="gemini-3-flash-preview",
+            config={
+                "system_instruction": system_prompt
+            }
+        )
+
+    def send(self, user_message: str) -> str:
+        response = self.chat.send_message(user_message)
+        return response.text
+
+bot = Wrapper()
+while True:
+    prompt = input("Enter a prompt or exit to speak to InsideOut: ")
+    if prompt.lower() == "exit":
+        break
+    else:
+        print(bot.send(prompt))
